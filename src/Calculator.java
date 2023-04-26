0d0f101ac6ef14db7f3c1af5f08aea4baecbe59c
LucasLopesMoreira <englucastrab@gmail.com>
2023-04-26 02:59:51 -0300

    first push

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+
+        System.out.print("Enter the second number: ");
+
+        
+
+    }
+}
